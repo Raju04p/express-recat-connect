26f9147eda6560368c6a3ebc679b559ff459a19d
Raju04p <154728176+Raju04p@users.noreply.github.com>
2024-02-28 15:51:55 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,4 +12,35 @@ app.get( "/api", (req,res) => {
 
 app.listen( port, () => {
     console.log ("server start  in port 5000")
-})
+})//
+
+
+
+import React, {useEffect, useState } from 'react'
+
+function App() {
+
+  const [backentData, setbackendData] = useState([{}])
+  useEffect ( () => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setbackendData(data)
+      }
+    )
+  }, [])
+  return (
+    <div>
+      {(typeof backentData.user === 'undefined') ? (
+      <p>Loading..</p>
+      ): (
+        backentData.user.map((user,  i) => (
+        <p key= {i}>{user}</p>
+        ))
+      )}
+    </div>
+  )
+}
+
+export default App
